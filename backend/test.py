04ca48283ee33bdb77eed61b53ec032931607961
sud0su <rencongpc@gmail.com>
2019-05-13 11:28:45 +0700

    remove test file

--- backend/test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-with open(os.path.abspath(os.path.join('/Users/immap/Documents/Docker/Python/Django/django-ecommerce-api/backend', '..', '.env'))) as f:
-    mylist = [line.rstrip('\n') for line in f]
-    print(mylist)
